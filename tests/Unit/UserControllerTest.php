4902ca2f2dc043c7e097840873db33c7b247090e
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2023-12-30 17:53:25 +0900

    PHPUnitを用いて、UserControllerのテストコードを記載
    editメソッドのテストコードを記載

--- tests/Unit/UserControllerTest.php
+++ tests/Unit/UserControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\User;
+use App\Tag;
 use App\UserPrefectureMap;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -25,10 +26,42 @@ class UserControllerTest extends TestCase
 
         // ユーザー情報表示のリクエストを送信
         $response = $this->actingAs($user)->get(route('users.show', ['name' => $user->name]));
-        dd($response);
         // レスポンスの確認
         $response->assertStatus(200);
         $response->assertViewIs('users.show');
         $response->assertViewHas('user', $user);
+    }
+
+    /**
+     * editメソッドのテスト
+     * 
+     * @test
+     */
+    public function testEdit()
+    {
+        $user = User::factory()->create();
+
+        // 人気タグと関連記事のセットアップ
+        // TODO
+        // ここで Tag モデルと Article モデル、およびそれらの関連を適切に設定
+        // 
+        // $tag1 = Tag::factory()->create();
+        // $article1 = Article::factory()->create();
+        // $article1->tags()->attach($tag1);
+
+        $response = $this->actingAs($user)->get(route('users.edit', ['name' => $user->name]));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('users.edit');
+        $response->assertViewHas('user', function ($viewUser) use ($user) {
+            return $viewUser->id === $user->id;
+        });
+
+        // 人気タグの検証
+        $response->assertViewHas('tags', function ($viewTags) {
+            // getPopularTag メソッドの結果とビューに渡されたタグが一致するかを検証
+            $popularTags = Tag::getPopularTag();
+            return $popularTags->pluck('name')->sort()->values()->all() === $viewTags->pluck('name')->sort()->values()->all();
+        });
     }
 }
